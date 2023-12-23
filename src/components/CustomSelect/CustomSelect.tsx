9ec84872f13befca86730a945e3dd396bf84dbbc
maksymopanasenko <maxpanas008@gmail.com>
2023-12-23 16:50:44 +0100

    adjust search input in select

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -14,30 +14,46 @@ interface FormValues {
     pokemons: [];
 }
 
-type ValueTypes = {
+interface ValueTypes {
     id: number;
     name: string;
 }
 
 const CustomSelect = ({ options }: CustomSelectTypes) => {
     const [showOptions, setshowOptions] = useState(false);
-    const [selectedValue, setSelectedValue] = useState<{
-        id: number;
-        name: string;
-    } | null>(null);
-
+    const [selectedValue, setSelectedValue] = useState<ValueTypes | null>(null);
     const { values, setFieldValue } = useFormikContext<FormValues>();
+    const [searchTerm, setSearchTerm] = useState("");
+    const [searchResults, setSearchResults] = useState(options);
+
+    const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+        const term = event.target.value;
+        setSearchTerm(term);
+    };
+
+    const clearTerm = () => {
+        setSearchTerm("");
+    };
+
+    useEffect(() => {
+        const filteredResults = options?.filter(item =>
+            item.name.toLowerCase().includes(searchTerm.toLowerCase())
+        ) || null;
+
+        setSearchResults(filteredResults);
+    }, [searchTerm]);
 
     useEffect(() => {
         if (!selectedValue && values.pokemons.length >= 4) {
             setshowOptions(false);
+            setSearchTerm("");
         }
 
         const handler = (e: MouseEvent) => {
             if (!(e.target as HTMLElement)?.closest('#select') && !(e.target as HTMLElement)?.closest('#btn')) {
-                console.log(e.target);
 
                 setshowOptions(false);
+                setSearchTerm("");
             }
         };
         window.addEventListener("click", handler);
@@ -77,17 +93,15 @@ const CustomSelect = ({ options }: CustomSelectTypes) => {
         }, 0);
     }
 
-    const onOptionClick = (currentOption: {
-        id: number;
-        name: string;
-    }) => {
+    const onOptionClick = (currentOption: ValueTypes) => {
         setSelectedValue(currentOption);
     };
-
 
     const capitalizeName = (name: string) => {
         return name[0].toUpperCase() + name.slice(1);
     }
+
+    const renderedOptions = searchResults ?? options;
 
     return (
         <div id="select" tabIndex={0} className="border-slate-400 focus:outline-none focus:border-purple-500 border-solid border-2 rounded relative">
@@ -113,8 +127,26 @@ const CustomSelect = ({ options }: CustomSelectTypes) => {
             </div>
             {showOptions && (
                 <div className="absolute w-full z-50 left-0 top-full">
+                    <div className="flex justify-between items-center bg-gray-600 w-full border border-solid border-purple-400">
+                        <div className="p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#fff" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                            </svg>
+                        </div>
+
+                        <div className="w-full relative">
+                            <input className="text-xl w-full p-2 placeholder:text-slate-400" placeholder="Type to find" value={searchTerm} onChange={handleSearch} />
+                            {searchTerm && (
+                                <span className='flex justify-end cursor-default absolute right-2 top-3 bg-gray-200 rounded' onClick={clearTerm}>
+                                    <svg id="btn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                </span>
+                            )}
+                        </div>
+                    </div>
                     <div className="w-full max-h-52 overflow-auto">
-                        {options?.map((option) => (
+                        {renderedOptions?.map((option) => (
                             <div
                                 key={option.id}
                                 className={`text-center text-xl cursor-pointer px-6 py-4 bg-violet-100 border-b border-solid border-purple-400 hover:bg-violet-300 ${values.pokemons.map((val: ValueTypes) => val.name).includes(option.name) ? "text-orange-600 hover:text-orange-600" : " hover:text-white"}`}
